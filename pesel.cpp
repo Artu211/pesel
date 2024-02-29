ec0639b440eb238e32647a7e0352e7987b7500b4
Artu211 <160549068+Artu211@users.noreply.github.com>
2024-02-29 21:58:00 +0100

    Update pesel.cpp

--- pesel.cpp
+++ pesel.cpp
@@ -14,7 +14,7 @@ int main()
         cin>>strPesel;
         for (int i=0; i<strPesel.length(); i++)
         {
-            pesel[i]=strPesel[i]; // dla kazdej cyfry ze stringa zamiana przydzielona szufladka w tablicy
+            pesel[i]=strPesel[i]; // każda cyfra z stringa dostaje szufladke w tablicy
         }
         int suma = pesel[0]1 + pesel[1]3 + pesel[2]7 + pesel[3]9 + pesel[4]1 + pesel[5]3 +
                    pesel[6]7 + pesel[7]9 + pesel[8]1 + pesel[9]3 + pesel[10]1;
