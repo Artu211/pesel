162d7270cee47a408dc30206cce2119a89381f47
Artu211 <160549068+Artu211@users.noreply.github.com>
2024-02-29 21:59:06 +0100

    Update pesel.cpp

--- pesel.cpp
+++ pesel.cpp
@@ -16,8 +16,8 @@ int main()
         {
             pesel[i]=strPesel[i]; // każda cyfra z stringa dostaje szufladke w tablicy
         }
-        int suma = pesel[0]1 + pesel[1]3 + pesel[2]7 + pesel[3]9 + pesel[4]1 + pesel[5]3 +
-                   pesel[6]7 + pesel[7]9 + pesel[8]1 + pesel[9]3 + pesel[10]1;
+        int suma = pesel[0]*1 + pesel[1]*3 + pesel[2]*7 + pesel[3]*9 + pesel[4]*1 + pesel[5]*3 +
+                   pesel[6]*7 + pesel[7]*9 + pesel[8]*1 + pesel[9]*3 + pesel[10]*1;
 
         if(suma > 0)
         {
